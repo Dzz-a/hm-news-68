6790d0eef2b855e19a2a5598a545c07e03e2e74d
Dzz-a <515953914@qq.com>
2020-08-27 16:54:39 +0800

    配置了项目中的vant

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/base.less'
+import './styles/iconfont.css'
+// 全局导入vant
+import Vant from 'vant'
+
+import 'vant/lib/index.css'
+Vue.use(Vant)
+// 按需加载vant
+// import { Button } from 'vant'
+// Vue.use(Button)
 
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
